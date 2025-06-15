3ae32209d9b2fbad3432256ed369e2dcfdc7bb37
장정환(JH) <jhjang@woowahan.com>
2025-06-15 11:42:25 +0900

    배달이 표시, 랜덤 선물표시, 타이머 설정

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
-var keyPressEventHandler = function (event) {
-    console.log('Key pressed:', event);
+var movedLocation = {
+    x: 0,
+    y: 0,
 };
+var keyPressEventHandler = function (event) {
+    var baedal = document.querySelector('.baedal');
+    if (!baedal)
+        return;
+    if (event.code === 'KeyW') {
+        movedLocation.y -= 10;
+        baedal.style.top = "calc(50vh - 48px + ".concat(movedLocation.y, "px)");
+    }
+    if (event.code === 'KeyA') {
+        movedLocation.x -= 10;
+        baedal.style.left = "calc(50vw - 48px + ".concat(movedLocation.x, "px)");
+    }
+    if (event.code === 'KeyS') {
+        movedLocation.y += 10;
+        baedal.style.top = "calc(50vh - 48px + ".concat(movedLocation.y, "px)");
+    }
+    if (event.code === 'KeyD') {
+        movedLocation.x += 10;
+        baedal.style.left = "calc(50vw - 48px + ".concat(movedLocation.x, "px)");
+    }
+};
+var createGift = function () {
+    var gift = document.createElement('img');
+    gift.src = 'images/gift.png';
+    var randomLocation = createRandomLocation();
+    gift.style.position = 'absolute';
+    gift.style.left = "".concat(randomLocation.x, "px");
+    gift.style.top = "".concat(randomLocation.y, "px");
+    document.body.appendChild(gift);
+};
+var createRandomLocation = function () {
+    var randomX = Math.floor(Math.random() * window.innerWidth - 64);
+    var randomY = Math.floor(Math.random() * window.innerHeight - 64);
+    while (randomX < 200 && randomY < 100) {
+        // Ensure the gift is not too close to the top-left corner
+        randomX = Math.floor(Math.random() * window.innerWidth - 64);
+        randomY = Math.floor(Math.random() * window.innerHeight - 64);
+    }
+    return {
+        x: randomX,
+        y: randomY,
+    };
+};
+var gameState = 'idle';
+var remainingTime = 10; // 60 seconds
 window.addEventListener('load', function () {
     var body = document.querySelector('body');
     body === null || body === void 0 ? void 0 : body.addEventListener('keypress', keyPressEventHandler);
+    gameState = 'playing';
+    var timer = setInterval(function () {
+        remainingTime -= 1;
+        createGift();
+        if (remainingTime <= 0) {
+            clearInterval(timer);
+            gameState = 'end';
+        }
+    }, 1000);
 });
--- index.ts
+++ index.ts
@@ -1,8 +1,71 @@
+const movedLocation = {
+  x: 0,
+  y: 0,
+};
 const keyPressEventHandler = (event: KeyboardEvent) => {
-  console.log('Key pressed:', event);
+  const baedal = document.querySelector<HTMLElement>('.baedal');
+  if (!baedal) return;
+
+  if (event.code === 'KeyW') {
+    movedLocation.y -= 10;
+    baedal.style.top = `calc(50vh - 48px + ${movedLocation.y}px)`;
+  }
+  if (event.code === 'KeyA') {
+    movedLocation.x -= 10;
+    baedal.style.left = `calc(50vw - 48px + ${movedLocation.x}px)`;
+  }
+  if (event.code === 'KeyS') {
+    movedLocation.y += 10;
+    baedal.style.top = `calc(50vh - 48px + ${movedLocation.y}px)`;
+  }
+  if (event.code === 'KeyD') {
+    movedLocation.x += 10;
+    baedal.style.left = `calc(50vw - 48px + ${movedLocation.x}px)`;
+  }
 };
 
+const createGift = () => {
+  const gift = document.createElement('img');
+  gift.src = 'images/gift.png';
+
+  const randomLocation = createRandomLocation();
+  gift.style.position = 'absolute';
+  gift.style.left = `${randomLocation.x}px`;
+  gift.style.top = `${randomLocation.y}px`;
+
+  document.body.appendChild(gift);
+};
+
+const createRandomLocation = () => {
+  let randomX = Math.floor(Math.random() * window.innerWidth - 64);
+  let randomY = Math.floor(Math.random() * window.innerHeight - 64);
+
+  while (randomX < 200 && randomY < 100) {
+    // Ensure the gift is not too close to the top-left corner
+    randomX = Math.floor(Math.random() * window.innerWidth - 64);
+    randomY = Math.floor(Math.random() * window.innerHeight - 64);
+  }
+
+  return {
+    x: randomX,
+    y: randomY,
+  };
+};
+
+type GameState = 'idle' | 'playing' | 'end';
+let gameState: GameState = 'idle';
+let remainingTime = 10; // 60 seconds
 window.addEventListener('load', () => {
   const body = document.querySelector('body');
   body?.addEventListener('keypress', keyPressEventHandler);
+
+  gameState = 'playing';
+  const timer = setInterval(() => {
+    remainingTime -= 1;
+    createGift();
+    if (remainingTime <= 0) {
+      clearInterval(timer);
+      gameState = 'end';
+    }
+  }, 1000);
 });
